812ad7f3204db4dea020396450cd3e6eb18ef8d0
CCARTON <chloe@lumapps.com>
2023-06-22 21:30:12 +0200

    fixup! feat(cors): add cors middleware

--- app/infrastructure/web_app.py
+++ app/infrastructure/web_app.py
@@ -9,18 +9,10 @@ from app.endpoints import predict
 
 def create_app() -> FastAPI:
     app = FastAPI(redoc_url="/redoc")
-    origins = [
-        "http://localhost",
-        "http://localhost:8080",
-        "http://localhost:9580",
-        "http://100.125.89.106:8080",
-        "http://100.125.89.106:9580",
-
-    ]
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
