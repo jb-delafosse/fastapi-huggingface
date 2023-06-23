e8d64c1ede9d2bae43354470aefb939773c43585
CCARTON <chloe@lumapps.com>
2023-06-23 10:37:12 +0200

    feat(guess-answer): add posts & sources

--- app/usecase/answers.py
+++ app/usecase/answers.py
@@ -1,11 +1,12 @@
+import logging
 from typing import List
 
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, RetrievalQAWithSourcesChain
 from langchain.llms import AzureOpenAI
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import LLMChainFilter
 
-from app.usecase.comments import get_comments
+from app.usecase.comments import get_comments_and_posts
 from langchain.docstore.document import Document
 from langchain.schema import BaseRetriever
 
@@ -23,17 +24,24 @@ class PotentialAnswersRetriever(BaseRetriever):
 
 
 def get_comments_as_documents(user_jwt: str, query: str) -> list[Document]:
-    comments = get_comments(user_jwt, query)
+    comments_and_posts = get_comments_and_posts(user_jwt, query)
+
     return [
         Document(
             page_content=next(iter(comment.get("text").values())),
-            metadata={"comment_id": comment.get("uid"), "post_id": comment.get("content")}
+            metadata={"comment_id": comment.get("uid"), "post_id": comment.get("content"), "source": comment.get("content")}
         )
-        for comment in comments if comment.get("text")
+        for comment in comments_and_posts["comments"] if comment.get("text")
+    ] + [
+        Document(
+            page_content=post.text,
+            metadata={"comment_id": None, "post_id": post.id, "source": post.id}
+        )
+        for post in comments_and_posts["posts"] if post.text
     ]
 
 
-def get_answer(user_jwt: str, query: str, use_prefilter: bool = True) -> str:
+def get_answer(user_jwt: str, query: str, use_prefilter: bool = False) -> str:
     llm = AzureOpenAI(deployment_name="matthias", model_name="text-davinci-003")
 
     retriever = PotentialAnswersRetriever(user_jwt)
@@ -44,5 +52,7 @@ def get_answer(user_jwt: str, query: str, use_prefilter: bool = True) -> str:
             base_retriever=retriever,
         )
 
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
-    return qa.run(query)
+    qa = RetrievalQAWithSourcesChain.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+    answer = qa(query)
+    logging.warning(answer)
+    return answer
--- app/usecase/comments.py
+++ app/usecase/comments.py
@@ -1,8 +1,9 @@
-from app.adapter.monolite_api import get_post_comments
+import logging
+from app.adapter.monolite_api import get_post_comments, get_posts
 from app.usecase.similar_content import get_similar_content
 
 
-def get_comments(
+def get_comments_and_posts(
     user_jwt: str,
     query: str
 ) -> list[dict]:
@@ -11,5 +12,10 @@ def get_comments(
     the top-level comments to the X most similar
     posts.
     """
-    similar_posts = get_similar_content(query)
-    return get_post_comments(user_jwt, [post.id for post in similar_posts])
+    similar_posts = list(get_similar_content(query))
+    comments = get_post_comments(user_jwt, [post.id for post in similar_posts])
+
+    return {
+        "posts": similar_posts,
+        "comments": comments
+    }
